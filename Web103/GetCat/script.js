149ce20226b6372828a242e860ed2715e59c2761
Joseph Maxwell <joseph.maxwell@hyland.com>
2024-07-08 13:13:41 -0400

    update get cat

--- Web103/GetCat/script.js
+++ Web103/GetCat/script.js
@@ -1,9 +1,47 @@
+async function speak(message) {
+  let ttsUrl = `https://tiktok-tts.weilnet.workers.dev/api/generation`;
+  let body = {
+    text: message,
+    voice: 'en_us_rocket'
+  }
+
+  try {
+    let response = await fetch(ttsUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    });
+
+    let responseJson = await response.json();
+    let audioUrl = `data:audio/wav;base64,${responseJson.data}`;
+    let audio = new Audio(audioUrl);
+
+    setTimeout(() => audio.play(), 1000);
+
+  } catch (e) {
+    alert(e);
+    return;
+  }
+}
+
+function randomColorVal() {
+  return Math.floor(Math.random() * 256);
+}
+
 async function run() {
   let catPic = document.querySelector("#cat-pic");
+  catPic.src = "https://media.tenor.com/o8m3bKTsifUAAAAM/hold-on.gif";
 
   let messageInput = document.querySelector("#cat-message");
   let message = messageInput.value;
-  let size = 40 + Math.random() * 10;
 
-  catPic.src = `https://cataas.com/cat/says/${message}?font=Comic%20Sans%20MS&fontSize=${size}&fontColor=%23FFF&fontBackground=%23000&filter=mono&position=bottom`;
+  let r = randomColorVal();
+  let g = randomColorVal();
+  let b = randomColorVal();
+
+  catPic.src = `https://cataas.com/cat/says/${message}?fontSize=50&fontColor=white&filter=custom&r=${r}&g=${g}&b=${b}`;
+
+  await speak(message);
 }
